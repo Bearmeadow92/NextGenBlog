14f8def7d7ca7a8a359c98d67589580902602c42
Bearmeadow92 <Jadamsfletcher@gmail.com>
2025-09-14 18:30:45 -0700

    Admin Internal mail system database fixes

--- api/messages.js
+++ api/messages.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const { Message } = require('../models/Post');
-const { authenticateToken } = require('../middleware/middleware');
+const jwt = require('jsonwebtoken');
+
+// Simple auth check function (inline to avoid import issues)
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ error: 'Access token required' });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) {
+            return res.status(403).json({ error: 'Invalid or expired token' });
+        }
+        req.user = user;
+        next();
+    });
+}
+
 const router = express.Router();
 
 // Get all messages (excluding archived by default)
